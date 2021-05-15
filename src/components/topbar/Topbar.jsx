8f5e34fad12cca966fca5ae21ba0c9ef50aa1431
yomex4life <yomex4life2001@gmail.com>
2021-05-15 16:39:56 +0100

    Complteted the menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,32 @@
 import './topbar.scss'
+import {Person, Mail} from '@material-ui/icons'
 
-export default function Topbar() {
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+    
     return (
-        <div className='topbar'>
-            
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">yomi</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+44545454545</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>specs@yomi.com</span>
+                    </div>
+                </div>
+                
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
